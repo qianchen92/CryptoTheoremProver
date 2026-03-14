af82884b59c5fde2c4bc9d2b22bceeebb69e0a17
qianchen92 <qianchen.314@gmail.com>
2026-03-14 14:45:19 +0800

    update

--- Crypto/Core/CostStructure/Cost.lean
+++ Crypto/Core/CostStructure/Cost.lean
@@ -8,20 +8,19 @@ structure Traced (α : Type u) where
 deriving Repr, DecidableEq
 
 class HasAddCost (α : Type u) where
-  addCost : Nat := 1
+  addCost : Nat
 
 class HasMulCost (α : Type u) where
-  mulCost : Nat := 1
+  mulCost : Nat
 
 class HasNegCost (α : Type u) where
-  negCost : Nat := 1
-
+  negCost : Nat
 class HasSMulCost (R : Type uR) where
   smulCost : R → Nat
 
 namespace Traced
 
-def pure {α : Type u} (a : α) : Traced α :=
+def ofVal {α : Type u} (a : α) : Traced α :=
   ⟨a, 0⟩
 
 def add {α : Type u} [Add α] [HasAddCost α]
@@ -39,6 +38,29 @@ def neg {α : Type u} [Neg α] [HasNegCost α]
 def smul {R : Type uR} {α : Type u} [SMul R α] [HasSMulCost R]
     (r : R) (a : Traced α) : Traced α :=
   ⟨r • a.val, a.cost + HasSMulCost.smulCost (R := R) r⟩
+
+instance {α : Type u} [Add α] [HasAddCost α] : Add (Traced α) where
+  add := Traced.add
+
+instance {α : Type u} [Neg α] [HasNegCost α] : Neg (Traced α) where
+  neg := Traced.neg
+
+instance {α : Type u} [Mul α] [HasMulCost α] : Mul (Traced α) where
+  mul := Traced.mul
+
+instance {R : Type uR} {α : Type u} [SMul R α] [HasSMulCost R] : SMul R (Traced α) where
+  smul := Traced.smul
+
+@[simp] theorem ofVal_val {α : Type u} (a : α) : (ofVal a).val = a := rfl
+
+@[simp] theorem ofVal_cost {α : Type u} (a : α) : (ofVal a).cost = 0 := rfl
+
+@[simp] theorem add_val {α : Type u} [Add α] [HasAddCost α]
+    (a b : Traced α) : (Traced.add a b).val = a.val + b.val := rfl
+
+@[simp] theorem add_cost {α : Type u} [Add α] [HasAddCost α]
+    (a b : Traced α) :
+    (Traced.add a b).cost = a.cost + b.cost + HasAddCost.addCost (α := α) := rfl
 
 end Traced
 end Crypto.Core.CostStructure
--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # Crypto
 
-## GitHub configuration
+## Complexity Traced structure
 
-To set up your new GitHub repository, follow these steps:
-
-* Under your repository name, click **Settings**.
-* In the **Actions** section of the sidebar, click "General".
-* Check the box **Allow GitHub Actions to create and approve pull requests**.
-* Click the **Pages** section of the settings sidebar.
-* In the **Source** dropdown menu, select "GitHub Actions".
-
-After following the steps above, you can remove this section from the README file.
+We give the algebraic structure that tacks the complexity. For now we support adding operation cost for add, multiply, scaler product. 
